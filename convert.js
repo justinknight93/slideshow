b1ce10327b0d2b6f4316cf6be8267d1b275b2e4a
ethan.rosenberg@omnifederal.com <ethan.rosenberg@omnifederal.com>
2025-12-05 14:07:58 -0800

    begining of formatting implementation

--- convert.js
+++ convert.js
@@ -56,22 +56,77 @@ function extractTextFromNode(node) {
   return text.trim();
 }
 
+/**
+ * @param {Element} span <a:r> element containing span information
+ * @returns {string} formatted span as html
+ */
+function formatSpan(span) {
+  let begin = "";
+  let end = "";
+
+  const addTag = (tag) => {
+    begin += `<${tag}>`;
+    end = `</${tag}>` + end;
+  };
+
+  const formatting = span.getElementsByTagName("a:rPr")[0];
+  if (formatting) {
+    if (parseInt(formatting.getAttribute("b"))) addTag("b");
+    if (parseInt(formatting.getAttribute("i"))) addTag("i");
+    if (formatting.getAttribute("u") && formatting.getAttribute("u") !== "none")
+      addTag("u");
+  }
+
+  const textContent = span.getElementsByTagName("a:t")[0]?.textContent;
+
+  return begin + textContent + end;
+}
+
+/**
+ * @param {Element} line <a:p> element containing line information
+ * @returns {string} formatted line as html
+ */
+function formatLine(line) {
+  let str = "";
+  const formatting = line.getElementsByTagName("a:pPr")[0];
+
+  if (formatting) {
+    const indent = parseInt(formatting.getAttribute("lvl"));
+    if (indent) str += "&#9;".repeat(indent) + "&#x2022;";
+  }
+
+  const spans = line.getElementsByTagName("a:r");
+
+  for (let i = 0; i < spans.length; ++i) {
+    str += " " + formatSpan(spans[i]);
+  }
+
+  return "<p>" + str + "</p>";
+}
+
+/**
+ * @param {JSZip} zip
+ * @returns {{slide: number, notes: string}[]}
+ */
 async function extractNotes(zip) {
   const notes = [];
 
   for (const filename of Object.keys(zip.files)) {
-    if (filename.startsWith("ppt/notesSlides/") && filename.endsWith(".xml")) {
+    if (/notesSlide[0-9]+/i.test(filename)) {
       const xml = await zip.file(filename).async("string");
       const dom = new DOMParser().parseFromString(xml, "text/xml");
+      const textContent = dom.getElementsByTagName("p:txBody")[0];
+      if (!textContent) continue;
+
+      let slideText = "";
+
+      const lines = textContent.getElementsByTagName("a:p");
+      for (let i = 0; i < lines.length; ++i) {
+        slideText += formatLine(lines[i]);
+      }
 
       const match = filename.match(/notesSlide(\d+)\.xml$/);
       const slideNumber = match ? parseInt(match[1], 10) : -1;
-
-      const textNodes = dom.getElementsByTagName("a:t");
-      let slideText = "";
-      for (let i = 0; i < textNodes.length; i++) {
-        slideText += textNodes[i].textContent + "\n";
-      }
 
       notes.push({
         slide: slideNumber,
